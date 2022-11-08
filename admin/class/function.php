7f069e76e5161b843d5552125ebcf011118807aa
Sabbir Ahmed <sabbirahmed2013303@gmail.com>
2022-11-08 13:26:45 +0600

    10th days work :3

--- admin/class/function.php
+++ admin/class/function.php
@@ -22,6 +22,22 @@
             }
 
         }
+
+        public function admin_login($data){
+            $admin_email = $data['admin_email'];
+            $admin_pass = md5($data['admin_pass']);
+
+            $query = "SELECT * FROM admin_info WHERE admin_email = '$admin_email' && admin_pass = '$admin_pass'";
+
+            if(mysqli_query($this->conn, $query)){
+                $admin_info = mysqli_query($this->conn, $query);
+                if($admin_info){
+                    header("location: dashboard.php");
+                }else{
+                    header("location: dashboard.php");
+                }
+            }
+        }
     }
 
 ?>
